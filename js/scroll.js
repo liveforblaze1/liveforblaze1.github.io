ed158477be25ed6f6377138ed3049376b8b841ad
liveforblaze1 <39407886+liveforblaze1@users.noreply.github.com>
2018-07-14 01:27:28 +0300

    Add files via upload

--- js/scroll.js
+++ js/scroll.js
@@ -7,9 +7,31 @@ window.onscroll = function() {
   }
 }
 
+window.onload = function () {
+    document.body.onclick = function (e) {
+        e = e || event;
+        target = e.target || e.srcElement;
+        if (target.id == "block" || target.id == "block-child" || target.id == "block-child2") {
+          
+        } else {
+              var block = document.getElementById('block');
+              console.log("HideBlock");
+              block.className = "block";
+              setTimeout(function() { block.style.display = "flex"}, 300);
+        }
+    }
+}
+
 function showBlock() {
   var block = document.getElementById('block');
   console.log("ShowBlock");
   block.style.display = "flex";
   setTimeout(function() { block.className += " animatedBlock" }, 300);
+}
+
+function hideBlock() {
+  var block = document.getElementById('block');
+  console.log("HideBlock");
+  block.className = "block";
+  setTimeout(function() { block.style.display = "flex"}, 300);
 }
