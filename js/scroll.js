6e82ed5c02dcdd4394e9a36cfb7c11250eeb39a4
liveforblaze1 <39407886+liveforblaze1@users.noreply.github.com>
2018-07-14 02:10:37 +0300

    Add files via upload

--- js/scroll.js
+++ js/scroll.js
@@ -1,3 +1,4 @@
+//Navbar scroll
 window.onscroll = function() {
   var scrolled = window.pageYOffset || document.documentElement.scrollTop;
   if (document.documentElement.scrollTop == 0) {
@@ -7,12 +8,33 @@ window.onscroll = function() {
   }
 }
 
+
+//Hide block onkeypress
+window.addEventListener("keydown", function (event) {
+  if (event.defaultPrevented) {
+    return; // Do nothing if the event was already processed
+  }
+  switch (event.key) {
+    case "Escape":
+      var block = document.getElementById('block');
+      console.log("HideBlock");
+      block.className = "block";
+      setTimeout(function() { block.style.display = "flex"}, 300);
+      break;
+    default:
+      return; // Quit when this doesn't handle the key event.
+  }
+  // Cancel the default action to avoid it being handled twice
+  event.preventDefault();
+}, true);
+
+//Hide block onClick outside
 window.onload = function () {
     document.body.onclick = function (e) {
         e = e || event;
         target = e.target || e.srcElement;
         if (target.id == "block" || target.id == "block-child" || target.id == "block-child2") {
-          
+
         } else {
               var block = document.getElementById('block');
               console.log("HideBlock");
