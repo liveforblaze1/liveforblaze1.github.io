243f3eb018164ea1f018f6df212d2128c93306c7
liveforblaze1 <39407886+liveforblaze1@users.noreply.github.com>
2018-07-14 04:58:37 +0300

    Add files via upload

--- js/scroll.js
+++ js/scroll.js
@@ -16,10 +16,17 @@ window.addEventListener("keydown", function (event) {
   }
   switch (event.key) {
     case "Escape":
-      var block = document.getElementById('block');
-      console.log("HideBlock");
-      block.className = "block";
-      setTimeout(function() { block.style.display = "flex"}, 300);
+      var block1= document.getElementById('block1');
+      block1.className = "block";
+      var block2= document.getElementById('block2');
+      block2.className = "block";
+    //  setTimeout(function() { block.style.display = "flex"}, 300);
+      var b1 = document.getElementById('b1');
+      var b2 = document.getElementById('b2');
+      var b3 = document.getElementById('b3');
+      b1.className = "first-block";
+      b2.className = "first-block";
+      b3.className = "first-block";
       break;
     default:
       return; // Quit when this doesn't handle the key event.
@@ -33,27 +40,36 @@ window.onload = function () {
     document.body.onclick = function (e) {
         e = e || event;
         target = e.target || e.srcElement;
-        if (target.id == "block" || target.id == "block-child" || target.id == "block-child2") {
+        if (target.id == "block1" || target.id == "block2" || target.id == "block-child" || target.id == "block-child2") {
 
         } else {
-              var block = document.getElementById('block');
-              console.log("HideBlock");
-              block.className = "block";
-              setTimeout(function() { block.style.display = "flex"}, 300);
+              var block1 = document.getElementById('block1');
+              block1.className = "block";
+              var block2 = document.getElementById('block2');
+              block2.className = "block";
+              var b1 = document.getElementById('b1');
+              var b2 = document.getElementById('b2');
+              var b3 = document.getElementById('b3');
+              b1.className = "first-block";
+              b2.className = "first-block";
+              b3.className = "first-block";
         }
     }
 }
 
-function showBlock() {
-  var block = document.getElementById('block');
-  console.log("ShowBlock");
+//onClick show block
+function showBlock(a) {
+  var block = document.getElementById(`block${a}`);
+  console.log(block);
   block.style.display = "flex";
   setTimeout(function() { block.className += " animatedBlock" }, 300);
-}
 
-function hideBlock() {
-  var block = document.getElementById('block');
-  console.log("HideBlock");
-  block.className = "block";
-  setTimeout(function() { block.style.display = "flex"}, 300);
+  var b1 = document.getElementById('b1');
+  var b2 = document.getElementById('b2');
+  var b3 = document.getElementById('b3');
+  setTimeout(function() {
+    b1.className += " firstScreen-hide";
+    b2.className += " firstScreen-hide";
+    b3.className += " firstScreen-hide";
+  }, 300);
 }
